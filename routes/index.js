87e8438e6e14da8f7a08cefe6d963f1e83dfd803
colegaud@gmail.com <colegaud@gmail.com>
2020-02-26 17:46:42 -0800

    Added temporary word sort

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,20 @@ const getResults = require("../scraper");
 /* GET home page. */
 router.get("/", async function(req, res, next) {
   const result = await getResults();
+  result.categories = wordSort(result.categories);
   res.render("index", result);
 });
+function wordSort(paralist) {
+  wordArr = [];
+  for(let i = 0; i < paralist.length; i++) {
+    let wordlist = paralist[i].split(" ");
+    for(let j = 0; j < wordlist.length; j++) {
+
+      if(wordlist[j].charAt(0) == wordlist[j].charAt(0).toUpperCase() && wordlist[j].charAt(0).match(/[a-z]/i)) {
+        wordArr.push(wordlist[j]);
+      }
+    }
+  }
+  return wordArr;
+}
 module.exports = router;
