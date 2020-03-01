368b735ed4bd29d3f450d501e26884ee8bc77d8b
colegaud@gmail.com <colegaud@gmail.com>
2020-03-01 13:05:08 -0800

    Added insertion into database if movie doesn't exist already

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,42 @@ const router = express.Router();
 const getResults = require("../scraper");
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
-const client = new MongoClient(process.env.DATABASE_URL, { useNewUrlParser: true });
 
-client.connect(err => {
-  const collection = client.db("test").collection("myCollection");
-  var stream = collection.find({mykey:{$ne:2}}).stream();
-  stream.on("data", function(item) {});
-  stream.on("end", function() {});
-  console.log(stream);
-  client.close();
-});
 
-router.get("/", async function(req, res, next) {
+
+router.get("/", async function (req, res, next) {
   const result = await getResults();
   result.categories = wordSort(result.categories);
+  let resultCategories = result.categories;
+  MongoClient.connect(process.env.DATABASE_URL, (err, db) => {
+    if (err) throw err;
+    const testDB = db.db("test");
+    let exists = testDB.collection("testCollection").findOne({ name: result.siteName }).then(function (results) {
+      if (results == null) {
+        let doc = { name: result.siteName, result: resultCategories };
+        testDB.collection("testCollection").insertOne(doc, function (err, res) {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+          if (res)
+            console.log("Inserting data from " + result.siteName);
+          db.close();
+        });
+      }
+      else
+        console.log(result.siteName + " already exists!");
+    });
+  });
   res.render("index", result);
 });
 function wordSort(paralist) {
   wordArr = [];
-  for(let i = 0; i < paralist.length; i++) {
+  for (let i = 0; i < paralist.length; i++) {
     let wordlist = paralist[i].split(" ");
     let sentencelist = paralist[i].split(".");
-    for(let j = 0; j < wordlist.length; j++) {
-      if((wordlist[j].charAt(0) == wordlist[j].charAt(0).toUpperCase() && wordlist[j].charAt(0).match(/[a-z]/i)) || wordlist[j].length >= 6) {
+    for (let j = 0; j < wordlist.length; j++) {
+      if ((wordlist[j].charAt(0) == wordlist[j].charAt(0).toUpperCase() && wordlist[j].charAt(0).match(/[a-z]/i)) || wordlist[j].length >= 6) {
         wordArr.push(wordlist[j]);
       }
     }
